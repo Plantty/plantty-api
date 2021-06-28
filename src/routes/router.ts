4b1e49ff57020cb8404b14bbea0d7a4fc5753259
neexzera <specialforce656@gmail.com>
2021-06-28 19:59:53 -0300

    feat: adicionando a rota dos logs

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import plantRoutes from './plants.routes';
+import logsRoutes from './logs.routes';
 
 const router = Router();
 
 router.use('/plants', plantRoutes);
+router.use('/logs', logsRoutes);
 
 export default router;
