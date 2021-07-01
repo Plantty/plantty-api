49206671f17ba38e4ca923cc9a14c30e435f0767
neexzera <specialforce656@gmail.com>
2021-07-01 00:00:08 -0300

    feat: adicionando a rota de adminUser

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
-import plantRoutes from './plants.routes';
-import logsRoutes from './logs.routes';
-import userRoutes from './user.routes';
+import { Router } from "express";
+import plantRoutes from "./plants.routes";
+import logsRoutes from "./logs.routes";
+import userRoutes from "./user.routes";
+import adminUserRoutes from "./adminUser.routes";
 
 const router = Router();
 
-router.use('/plants', plantRoutes);
-router.use('/logs', logsRoutes);
-router.use('/users', userRoutes);
+router.use("/plants", plantRoutes);
+router.use("/logs", logsRoutes);
+router.use("/users", userRoutes);
+router.use("/adminUsers", adminUserRoutes);
 
 export default router;
