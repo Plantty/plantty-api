395bf12ddc9b62d37cc23a3cf9206e16828e8b89
Arthur Luciano <arthurluciano7@gmail.com>
2021-08-21 23:09:18 -0300

    feat: adicionando o middleware de upload de arquivo

--- src/routes/plants.routes.ts
+++ src/routes/plants.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import PlantController from "../controllers/PlantController";
+import { uploader } from "../middlewares/uploader";
 
 const router = Router();
 
 router.get("/", PlantController.index);
-router.post("/", PlantController.store);
+router.post("/", uploader.single('plantImage'), PlantController.store);
 
 router.get("/:search", PlantController.show);
 
